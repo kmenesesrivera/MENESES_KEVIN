33faecac5e0e14a20104d918d63f9afa088bb75e
KEVIN ARTHUR MENESES RIVERA <84211921+kmenesesrivera@users.noreply.github.com>
2024-08-12 21:43:12 -0500

    Correcciones

--- src/dao/impl/OdontologoDaoH2.java
+++ src/dao/impl/OdontologoDaoH2.java
@@ -10,87 +10,64 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OdontologoDaoH2 implements IDao<Odontologo> {
-    public static final Logger logger = Logger.getLogger(OdontologoDaoH2.class);
-    public static final String INSERT = "INSERT INTO ODONTOLOGO VALUES (DEFAULT,?,?);";
-    public static final String SELECT_ALL = "SELECT * FROM ODONTOLOGO ;";
+    private static final Logger logger = Logger.getLogger(OdontologoDaoH2.class);
+    private static final String INSERT = "INSERT INTO ODONTOLOGO(APELLIDO, NOMBRE) VALUES (?, ?)";
+    private static final String SELECT_ID = "SELECT * FROM ODONTOLOGO WHERE MATRICULA = ?";
+    private static final String SELECT_ALL = "SELECT * FROM ODONTOLOGO";
 
     @Override
     public Odontologo guardar(Odontologo odontologo) {
-        Connection connection = null;
         Odontologo odontologoARetornar = null;
-        try {
-            connection = H2Connection.getConnection();
-            connection.setAutoCommit(true);
-            PreparedStatement preparedStatement = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+
+        try (Connection connection = H2Connection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS)) {
+
             preparedStatement.setString(1, odontologo.getApellido());
             preparedStatement.setString(2, odontologo.getNombre());
             preparedStatement.executeUpdate();
-            connection.commit();
-            ResultSet resultSet = preparedStatement.getGeneratedKeys();
-            if (resultSet.next()) {
-                Integer id = resultSet.getInt(1);
-                odontologoARetornar = new Odontologo(id, odontologo.getApellido(), odontologo.getNombre());
-            }
-            logger.info("Odontologo persistido " + odontologoARetornar);
 
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-            e.printStackTrace();
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                logger.error(ex.getMessage());
-                e.printStackTrace();
-            } finally {
-                try {
-                    connection.setAutoCommit(true);
-                } catch (SQLException ex) {
-                    logger.error(ex.getMessage());
-                    e.printStackTrace();
+            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    Integer id = resultSet.getInt(1);
+                    odontologoARetornar = new Odontologo(id, odontologo.getApellido(), odontologo.getNombre());
                 }
             }
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                logger.error(e.getMessage());
-                e.printStackTrace();
-            }
+
+            logger.info("Odontologo persistido " + odontologoARetornar);
+
+        } catch (SQLException e) {
+            logger.error("Error al guardar el odontólogo: " + e.getMessage(), e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
+
         return odontologoARetornar;
     }
 
     @Override
-    public  List<Odontologo>  buscarTodos() {
-        Connection connection = null;
+    public List<Odontologo> buscarTodos() {
         List<Odontologo> listaOdontologos = new ArrayList<>();
-        try {
-            connection = H2Connection.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
+
+        try (Connection connection = H2Connection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            while (resultSet.next()) {
                 Integer idDB = resultSet.getInt(1);
                 String apellido = resultSet.getString(2);
                 String nombre = resultSet.getString(3);
                 listaOdontologos.add(new Odontologo(idDB, apellido, nombre));
             }
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-            e.printStackTrace();
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                logger.error(e.getMessage());
-                e.printStackTrace();
-            }
+
+        } catch (SQLException | ClassNotFoundException e) {
+            logger.error("Error al buscar todos los odontólogos: " + e.getMessage(), e);
         }
 
         logger.info("Listar Odontologos");
-        for(Odontologo odontologo: listaOdontologos){
+        for (Odontologo odontologo : listaOdontologos) {
             logger.info(odontologo);
         }
+
         return listaOdontologos;
     }
-
 }
